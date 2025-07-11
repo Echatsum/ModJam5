985779762885611829941f58c6a367c141f7fe46
AnonymousStrangerOW <169866456+AnonymousStrangerOW@users.noreply.github.com>
2025-07-10 22:34:44 -0400

    quantum puzzle complete!
    - the quantum puzzle in the hearthian section is now complete with no errors!
    - removed mountain king and entry of the fawns as per request from pikpik.
    - eye temple theme from eote now plays in the karvi ship interior
    - added collision to all the objects in the exhibits (including the stranger ufo and the hearthian props)

---
+++ QuantumPuzzle.cs
@@ -0,0 +1,91 @@
+﻿using NewHorizons.Utility;
+using OWML.Utils;
+using UnityEngine;
+
+namespace FifthModJam
+{
+    public class QuantumPuzzle : MonoBehaviour
+    {
+        [SerializeField]
+        private CustomItemSocket[] customItemSockets = new CustomItemSocket[2];
+        [SerializeField]
+        private GameObject[] quantumGeysers = new GameObject[4];
+        private bool hasCompletedQuantumPuzzle;
+
+        private void Start()
+        {
+            hasCompletedQuantumPuzzle = false;
+            SwapGeyser(0);
+        }
+
+        public void SwapGeyser(int index)
+        {
+            for (int i = 0; i < quantumGeysers.Length; i++)
+            {
+                if (i != index)
+                {
+                    quantumGeysers[i].SetActive(false);
+                } else
+                {
+                    quantumGeysers[i].SetActive(true);
+                }
+            }
+        }
+
+        private bool AreAllSocketsCorrect()
+        {
+            for (int i = 0; i < customItemSockets.Length; i++)
+            {
+                if (!customItemSockets[i].IsSocketOccupied())
+                {
+                    return false;
+                }
+            }
+
+            // If we haven't found any wrong socket, then they're all filled correctly
+            return true;
+        }
+
+        private bool AreAllSocketsEmpty()
+        {
+            for (int i = 0; i < customItemSockets.Length; i++)
+            {
+                if (customItemSockets[i].IsSocketOccupied())
+                {
+                    return false;
+                }
+            }
+
+            // If we haven't found any wrong socket, then they're all filled correctly
+            return true;
+        }
+
+        private void Update()
+        {
+            if (AreAllSocketsEmpty())
+            {
+                SwapGeyser(0);
+            }
+            else if (AreAllSocketsCorrect())
+            {
+                SwapGeyser(3);
+            }
+            else
+            {
+                for (int i = 0; i < customItemSockets.Length; i++)
+                {
+                    if (customItemSockets[i].IsSocketOccupied())
+                    {
+                        if (i == 0)
+                        {
+                            SwapGeyser(2);
+                        } else if (i == 1)
+                        {
+                            SwapGeyser(1);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
