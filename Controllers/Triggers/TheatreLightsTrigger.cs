04233373a13c64478b2dbe0fb1445c2103ef962b
Echatsum <Echatsum@protonmail.com>
2025-07-15 23:02:30 +0200

    added verifyParameters to the theaterLightTrigger

--- Controllers/Triggers/TheatreLightsTrigger.cs
+++ Controllers/Triggers/TheatreLightsTrigger.cs
@@ -7,6 +7,19 @@ namespace FifthModJam
         [SerializeField]
         private GameObject[] lights;
 
+        private void VerifyUnityParameters()
+        {
+            if (lights == null || lights.Length == 0)
+            {
+                FifthModJam.WriteLine("[TheatreLightsTrigger] light array is null or empty", OWML.Common.MessageType.Error);
+            }
+        }
+
+        private void Start()
+        {
+            VerifyUnityParameters();
+        }
+
         public virtual void OnTriggerEnter(Collider hitCollider)
         {
             // Checks if player collides with the trigger volume
@@ -21,7 +34,7 @@ namespace FifthModJam
 
         public virtual void OnTriggerExit(Collider hitCollider)
         {
-            // Checks if player collides with the trigger volume
+            // Checks if player no longer collides with the trigger volume
             if (hitCollider.CompareTag("PlayerDetector") && enabled)
             {
                 foreach (var light in lights)
