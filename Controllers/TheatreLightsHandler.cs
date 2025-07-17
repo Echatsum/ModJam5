5fe90c8febcab9392122e8f45fdd6a3c9ebb8ecc
AnonymousStrangerOW <169866456+AnonymousStrangerOW@users.noreply.github.com>
2025-07-16 21:29:25 -0400

    all dialogue added (except the bukrad stuff)
    - all dialogue has been inserted into the game as intended.
    - fixed a bug where the hearthian item sockets weren't taking all items
    - tractor beam puzzle is confirmed 100% done
    - all puzzles re-tested and function perfectly
    - crystal is now lowered, but it now goes to origin when you place it down. (seriously wtf)

---
+++ Controllers/TheatreLightsHandler.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace FifthModJam.Controllers
+{
+    public class TheatreLightsHandler : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject[] lights;
+        [SerializeField]
+        private SingleLightSensor slideTotemSensor;
+        private bool isOn;
+        private bool isOff;
+
+        public void Start()
+        {
+            isOn = false;
+            isOff = true;
+        }
+
+        private void Update()
+        {
+            if (!isOn && slideTotemSensor.IsIlluminated() && slideTotemSensor._lightSourceMask == LightSourceType.SIMPLE_LANTERN)
+            {
+                isOn = true;
+                isOff = false;
+                foreach (var light in lights)
+                {
+                    light.SetActive(true);
+                }
+            }
+
+            if (!isOff && !slideTotemSensor.IsIlluminated())
+            {
+                isOn = false;
+                isOff = true;
+                foreach (var light in lights)
+                {
+                    light.SetActive(false);
+                }
+            }
+        }
+    }
+}
