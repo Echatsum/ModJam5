f0ffe9b68c11ce6567b7fe83c01a657ec343c2f3
Echatsum <Echatsum@protonmail.com>
2025-07-20 02:05:28 +0200

    reworked the beam puzzle to include a lightSensor element

--- Controllers/TractorBeamPuzzle.cs
+++ Controllers/TractorBeamPuzzle.cs
@@ -13,6 +13,8 @@ namespace FifthModJam
         private SpeciesItemSocket _speciesItemSocket;
         [SerializeField]
         private GameObject _tractorBeam;
+        [SerializeField]
+        private LightSensor _lightSensor;
 
         private void VerifyUnityParameters()
         {
@@ -24,6 +26,10 @@ namespace FifthModJam
             {
                 FifthModJam.WriteLine("[TractorBeamPuzzle] tractorBeam is null", OWML.Common.MessageType.Error);
             }
+            if (_lightSensor == null)
+            {
+                FifthModJam.WriteLine("[TractorBeamPuzzle] lightsensor is null", OWML.Common.MessageType.Error);
+            }
         }
 
         private void Awake()
@@ -33,6 +39,11 @@ namespace FifthModJam
                 _speciesItemSocket.OnSocketablePlaced = (OWItemSocket.SocketEvent)Delegate.Combine(_speciesItemSocket.OnSocketablePlaced, new OWItemSocket.SocketEvent(OnSocketFilled));
                 _speciesItemSocket.OnSocketableRemoved = (OWItemSocket.SocketEvent)Delegate.Combine(_speciesItemSocket.OnSocketableRemoved, new OWItemSocket.SocketEvent(OnSocketRemoved));
             }
+            if (_lightSensor != null)
+            {
+                _lightSensor.OnDetectLight += new OWEvent.OWCallback(OnDetectLight);
+                _lightSensor.OnDetectDarkness += new OWEvent.OWCallback(OnDetectDarkness);
+            }
         }
         private void OnDestroy()
         {
@@ -40,6 +51,11 @@ namespace FifthModJam
             {
                 _speciesItemSocket.OnSocketablePlaced = (OWItemSocket.SocketEvent)Delegate.Remove(_speciesItemSocket.OnSocketablePlaced, new OWItemSocket.SocketEvent(OnSocketFilled));
                 _speciesItemSocket.OnSocketableRemoved = (OWItemSocket.SocketEvent)Delegate.Remove(_speciesItemSocket.OnSocketableRemoved, new OWItemSocket.SocketEvent(OnSocketRemoved));
+            }
+            if (_lightSensor != null)
+            {
+                _lightSensor.OnDetectLight -= new OWEvent.OWCallback(OnDetectLight);
+                _lightSensor.OnDetectDarkness -= new OWEvent.OWCallback(OnDetectDarkness);
             }
         }
 
@@ -54,16 +70,23 @@ namespace FifthModJam
         {
             CheckActivation();
         }
+        private void OnDetectLight()
+        {
+            CheckActivation();
+        }
         private void CheckActivation()
         {
-            if (_speciesItemSocket.HasCorrectSpeciesItem())
+            if (_speciesItemSocket.HasCorrectSpeciesItem() && _lightSensor.IsIlluminated())
             {
                 _tractorBeam?.SetActive(true);
                 Locator.GetShipLogManager().RevealFact("COSMICCURATORS_VOLCANO_BUILDING_TRACTORBEAM");
             }
         }
-
         private void OnSocketRemoved(OWItem item)
+        {
+            _tractorBeam?.SetActive(false);
+        }
+        private void OnDetectDarkness()
         {
             _tractorBeam?.SetActive(false);
         }
